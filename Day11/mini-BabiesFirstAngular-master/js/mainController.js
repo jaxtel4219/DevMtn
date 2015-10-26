2bda6046f4d1f743208c42994098c6f0549b11ff
Jack Schultz <jaxtel4219@gmail.com>
2015-10-26 15:53:47 -0600

    1st angular practice

---
+++ Day11/mini-BabiesFirstAngular-master/js/mainController.js
@@ -0,0 +1,15 @@
+var app = angular.module('friendsList');
+
+app.controller('mainController', function($scope){	
+
+$scope.friends = [
+	'Jeff',
+	'Billie',
+	'Brad',
+	'Pam',
+	'Karin',
+	'Renee'
+];
+
+}); //closes mainController
+
